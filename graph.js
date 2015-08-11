c570c7b2c53603f445a8a03cafe2b3e59471c839
nyarmith <powah.serge@gmail.com>
2015-08-11 06:16:41 -0400

    changed concepts related to rendering perspective

--- graph.js
+++ graph.js
@@ -1,11 +1,24 @@
-function graph(context){
-    this.drawAxes=function(width, height, pixels_per_x,pixels_per_y){
-        this.width=width;
-        this.height=height;
+function graph(context, width, height){
+    //pixel width and height
+    this.width = width;
+    this.height=height;
+
+    //set boundaries, which also sets scale
+    this.setBoundaries=function(x,y){
+        this.pixels_per_x=width/(x[1]-x[0]);
+        this.pixels_per_y=height/(y[1]-y[0]);
+        this.xBounds=x;
+        this.yBounds=y;
+    };
+
+    //set graph origin and width
+    this.drawAxes=function(){
+        //Determine where we start drawing
+        startx=(((width/2)/this.pixels_per_x)%1)*this.pixels_per_x;
+        starty=(((height/2)/this.pixels_per_y)%1)*this.pixels_per_y;
+
         //draw grid for unit scale
         context.beginPath();
-        startx=(((width/2)/pixels_per_x)%1)*pixels_per_x;
-        starty=(((height/2)/pixels_per_y)%1)*pixels_per_y;
         context.strokeStyle="gray";
         context.lineWidth=1;
         for(var i=startx; i < width; i+=pixels_per_x){
@@ -18,7 +31,6 @@ function graph(context){
             context.lineTo(width,i);
             context.stroke();
         }
-
         //axes drawing
         context.beginPath();
         context.strokeStyle="black";
@@ -32,8 +44,9 @@ function graph(context){
         context.lineTo(width/2,height);
         context.stroke();
     };
-    this.translate();
-    this.graph(func, domain, color){
+    //this.translate();
+    /*
+    this.graph=function(func, domain, color){
         N=20;
         step=(domain[1]-domain[0])/N;
         context.moveTo(domain[0]+width/2,);
@@ -41,12 +54,14 @@ function graph(context){
            context.beginPath();
         }
     }
+    */
 };
 
 window.onload = function(){
     canvas=document.getElementById("my_canvas");
     context=canvas.getContext("2d");
     var mygraph = new graph(context);
+    mygraph.setBoundaries([-4,4],[-4,4]);
     mygraph.drawAxes(canvas.width, canvas.height, 14, 14);
-    mygraph.graph(function(x){ return x*x; }, [-2,2], "red");
+    //mygraph.graph(function(x){ return x*x; }, [-2,2], "red");
 }
