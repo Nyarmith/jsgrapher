a04b6012e27a97f3e2ef0f3908c9a7e200e19d51
nyarmith <powah.serge@gmail.com>
2015-08-12 14:59:47 -0400

    finished spline as well as total overahul for arbitrary axis and orientation system!

--- graph.js
+++ graph.js
@@ -12,62 +12,48 @@ function graph(context, width, height){
     this.setBoundaries=function(x,y){
         this.pixels_per_x=width/Math.abs((x[1]-x[0]));
         this.pixels_per_y=height/Math.abs((y[1]-y[0]));
+        this.x_direction=Math.abs((x[1]-x[0]))/(x[1]-x[0]);
+        this.y_direction=Math.abs((y[1]-y[0]))/(y[1]-y[0]);
         this.xBounds=x;
         this.yBounds=y;
+        this.x_axis_loc=-1*this.y_direction*y[0];
+        this.y_axis_loc=-1*this.x_direction*x[0];
         this.drawGrid();
         this.drawAxes();
     };
 
+    //nested ifs for clarity at the cost of speed
     this.drawAxes=function(){
         //There x and y both have two options for their direction and placement
         //they should basically act as a reference point to where 0,0 axes are
         //relative to our function grid
         //axes drawing
-        var x_axis_loc=0;
-        var y_axis_loc=0;
 
-        this.x_direction=1;
-        //This will make y axis appear in middle of screen
-        if (this.xBounds[0]<0 && this.xBounds[1]>0){
-            y_axis_loc=Math.abs(this.xBounds[0])*this.pixels_per_x;
-        }
-        else if(this.xBounds[0]>0 && this.xBounds[1]<0){
-            y_axis_loc=Math.abs(this.xBounds[1])*this.pixels_per_x;
-            this.x_direction=-1;//reverse direction
-        }
-        else if(this.xBounds[0]<=0 && this.xBounds[1]<=0){  //both intervals are above x
-            y_axis_loc=width-1;
-        }
-        else{
-            y_axis_loc=1;
+        var x_draw=this.x_axis_loc*this.pixels_per_y;
+        var y_draw=this.y_axis_loc*this.pixels_per_x;
+
+        if (x_draw > this.height){
+            x_draw = this.height-1;
+        }else if(x_draw < 0){
+            x_draw = 1;
         }
 
-        this.y_direction=1;
-        //This will make x axis appear in middle of screen
-        if (this.yBounds[0]<0 && this.yBounds[1]>0){
-            //remember we draw from top left
-            x_axis_loc=Math.abs(this.yBounds[1])*this.pixels_per_y;
+        if (y_draw > this.width){
+            y_draw = this.width-1;
+        }else if(y_draw < 0){
+            y_draw = 1;
         }
-        else if(this.xBounds[0]>0 && this.xBounds[1]<0){
-            x_axis_loc=Math.abs(this.yBounds[0])*this.pixels_per_y;
-            this.y_direction=-1;
-        }
-        else if(this.yBounds[0]<=0 && this.yBounds[1]<=0){  //both intervals are above x
-            x_axis_loc=1;
-        }
-        else{
-            x_axis_loc=height-1;
-        }
+
         context.beginPath();
         context.strokeStyle="black";
         context.lineWidth=2;
         //draw main X
-        context.moveTo(0,x_axis_loc);
-        context.lineTo(width, x_axis_loc);
+        context.moveTo(0,x_draw);
+        context.lineTo(width, x_draw);
         context.stroke();
         //draw main Y
-        context.moveTo(y_axis_loc,0);
-        context.lineTo(y_axis_loc,height);
+        context.moveTo(y_draw,0);
+        context.lineTo(y_draw,height);
         context.stroke();
     }
     //set graph origin and width
@@ -87,6 +73,12 @@ function graph(context, width, height){
             context.stroke();
         }
     };
+
+
+    this.transform=function(x,y){
+        return [this.pixels_per_x*(this.y_axis_loc+this.x_direction*x), this.pixels_per_y*(this.x_axis_loc-this.y_direction*y)];
+    }
+
     //interpolates x and y array, pre-condition:x.length==y.length
     //TODO: Add discarding value for all out of bounds javascript more precise floatpoints, minus the first one, so our
     //spline at least goes to the border of our graph
@@ -97,12 +89,15 @@ function graph(context, width, height){
         }
         context.strokeStyle=color;
         context.lineWidth=1;
-        context.moveTo(x[0]*this.pixels_per_x,height-y[0]*this.pixels_per_y);
+        var t_v=this.transform(x[0],y[0]);
+        context.moveTo(t_v[0],t_v[1]);
         for (var i=1; i<x.length;i++){
-            context.lineTo((this.x_direction*x[i]-this.xBounds[0])*this.pixels_per_x,height+(this.yBounds[0]+this.y_direction*y[i])*this.pixels_per_y);
+            t_v=this.transform(x[i],y[i]);
+            context.lineTo(t_v[0],t_v[1]);
         }
         context.stroke();
     }
+
     //this.translate();
     /*
     this.graph=function(func, domain, color){
@@ -114,13 +109,15 @@ function graph(context, width, height){
         }
     }
     */
+
 };
 
 window.onload = function(){
     canvas=document.getElementById("my_canvas");
     context=canvas.getContext("2d");
     var mygraph = new graph(context, canvas.width, canvas.height);
-    mygraph.setBoundaries([-1,4],[-4,4]);
+    mygraph.setBoundaries([4,-1],[-4,4]);
     mygraph.lspline([-1,1,2,3,4],[2,1,3,-2,4],"red");
     //mygraph.graph(function(x){ return x*x; }, [-2,2], "red");
 }
+
